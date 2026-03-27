0e340ee9670e83dc1d2d35b4de5c8e1d81e88b7a
D' New x86 <mendieta.c.gamer@gmail.com>
2026-03-26 21:12:42 -0500

    .

--- Scripts/terminal.sh
+++ Scripts/terminal.sh
@@ -9,3 +9,39 @@ cls(){
 src(){
    source ~/.bashrc
 }
+
+
+youtube_chromium_base_app(){
+    chromium --app=https://www.youtube.com
+}
+
+youtube_chromium_min_base_app(){
+    chromium --app=https://www.youtube.com \
+    --process-per-site \
+    --disable-extensions \
+    --disable-sync \
+    --disable-notifications \
+    --disable-background-networking
+}
+
+
+youtube_chromium_tmp(){
+    chromium --app=https://www.youtube.com --user-data-dir=/tmp/youtube-app
+}
+
+
+youtube_chromium_limit_base(){
+    chromium --app=https://www.youtube.com \
+    --user-data-dir=/tmp/youtube-app \
+    --disable-extensions \
+    --force-effective-connection-type=2G \
+    --disable-sync \
+    --disable-notifications
+}
+
+
+youtube_chromium_exp(){
+    chromium --app=https://piped.video
+}
+
+
